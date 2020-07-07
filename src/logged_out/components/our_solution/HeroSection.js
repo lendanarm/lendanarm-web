91a314935c372f8bb7b2b4ffe05026e7e3de5748
Ifeanyi <doc3coder@gmail.com>
2020-07-07 01:27:06 +0100

    Youtube video

--- src/logged_out/components/our_solution/HeroSection.js
+++ src/logged_out/components/our_solution/HeroSection.js
@@ -151,7 +151,7 @@ function HeroSection(props) {
                       frameborder="0"
                       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                       allowfullscreen
-                      style={{ maxWidth: '100%', height: 'auto' }}
+                      style={{ maxWidth: '100%' }}
                     ></iframe>
                   </Grid>
                 </Box>

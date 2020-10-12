06c9afbfc0b34a4fdea9935f9235de620b762d9e
ifeanyimuogbo <doc3coder@gmail.com>
2020-10-12 01:30:27 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { unregister } from "./registerServiceWorker";
+import { unregister } from "./serviceWorker";
 unregister();
 
 ReactDOM.render(<App />, document.getElementById("root"));
